353c829de6eafc4797edbd65ca6061e88293cb0f
Krishna Kulkarni <krishna.kulkarni19@gmail.com>
2014-07-04 17:17:14 -0400

    Set up real iom_identity models

--- app/controllers/gold_standard_identities_controller.rb
+++ app/controllers/gold_standard_identities_controller.rb
@@ -5,20 +5,32 @@ class GoldStandardIdentitiesController < ApplicationController
   end
   
   def create
-    @gold_standard_identity = GoldStandardIdentity.new(gold_standard_identity_params)
-
+    @gold_standard_identity = IomIdentity.new
     if @gold_standard_identity.save
-      flash.now[:new_record_status] = "New gold_standard_identity Saved Successfully!"
-      @gold_standard_identity = GoldStandardIdentity.new
+      render json: @gold_standard_identity
     else
-      flash.now[:new_record_status] = "There was an error with your entry."
+      render json: @gold_standard_identity.errors
     end
-    
-    render :find_matches
   end
+  
+  # def create
+#     @gold_standard_identity = GoldStandardIdentity.new(gold_standard_identity_params)
+#
+#     if @gold_standard_identity.save
+#       flash.now[:new_record_status] = "New gold_standard_identity Saved Successfully!"
+#       @gold_standard_identity = GoldStandardIdentity.new
+#     else
+#       flash.now[:new_record_status] = "There was an error with your entry."
+#     end
+#
+#     render :find_matches
+#   end
 
   private
   def gold_standard_identity_params
-    params.require(:gold_standard_identity).permit(:first_name, :last_name, :age, :sex)
+    params.require(:gold_standard_identity).permit(
+    :first_name, :last_name, :alternate_name, :sex, :date_of_birth, 
+    :village_id, :group_id, :collective_id, :territory_id, :province_id,
+    :recorded_in_village_id, :recorded_by)
   end
 end
--- app/models/gold_standard_identity.rb
+++ app/models/gold_standard_identity.rb
@@ -2,4 +2,14 @@ class GoldStandardIdentity < ActiveRecord::Base
   belongs_to :household, inverse_of: :gold_standard_identities
   has_many :gold_standard_matches, inverse_of: :gold_standard_identity
   belongs_to :village, inverse_of: :gold_standard_identities
+  
+  #Alter implementation appropriately later
+  def iom_identity_matches
+    IomIdentity.all.select { |iom_identity| match_score(iom_identity) >= 3.5 }
+  end
+  
+  private
+  def match_score(iom_identity)
+    3.5
+  end
 end
