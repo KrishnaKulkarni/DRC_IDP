22e8029a082f64da60c4ae3bbdac767de70aeae3
Krishna Kulkarni <krishna.kulkarni19@gmail.com>
2014-07-05 14:18:52 -0400

    Set up router and identity show view

--- app/assets/javascripts/identity_matches.js
+++ app/assets/javascripts/identity_matches.js
@@ -1,22 +1,18 @@
 window.IdentityMatches = {
   Models: {},
   Collections: {},
+  Routers: {},
   Views: {},
   
   initialize: function() {    
-    // List sub-view construction
-    var indexView = new IdentityMatches.Views.IomIdentitiesIndex({
-      collection: IdentityMatches.Collections.iomIdentities
-    });
-    IdentityMatches.Collections.iomIdentities.fetch({
-      success: function() {
-        $("#found-matches").html(indexView.render().$el)
-      }
-    });
+
     
     // Search sub-view construction
     // var searchView = new IdentityMatches.Views.IomIdentitiesSearch();
     // $(".display-info-column").html(searchView.render().$el);
+  
+    new IdentityMatches.Routers.MatchesRouter();
+    Backbone.history.start();
   }
   
 };
