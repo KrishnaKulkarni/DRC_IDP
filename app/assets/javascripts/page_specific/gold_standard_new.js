1f1072f3288eb3c5ee1522be30f39e25d885bb92
Krishna Kulkarni <krishna.kulkarni19@gmail.com>
2014-07-06 00:04:14 -0400

    Add disable/blur functionality to registration forms

---
+++ app/assets/javascripts/page_specific/gold_standard_new.js
@@ -0,0 +1,61 @@
+$(function() {
+
+    // var $resourceSiteWebsite = $('input#resource_site_website');
+//
+//     $('input.required').validate(function(e) {
+//         return e.val().trim() != '';
+//     });
+//
+//     $().validate(function(e) {
+//         return (!!e.val().isURL()
+//             && e.val().indexOf('javascript:') == -1);
+//     });
+//
+//     $('#save').click(function() {
+//         var $invalidInputs = $('.invalid-input');
+//         if ($resourceSiteWebsite.val().indexOf('http') != 0) {
+//             $resourceSiteWebsite.val(
+//                 'http://' + $resourceSiteWebsite.val());
+//         }
+//         if (!!$invalidInputs.length) {
+//             $invalidInputs.addClass('force');
+//             vex.dialog.alert("Please fill out the highlighted fields.");
+//             return false;
+//         }
+//     });
+//
+//     var disableAvailability = function() {
+//         var checked = !!($(this).is(':checked'));
+//         $('.resource_site_available_date')
+//                 .fadeTo('slow', (0+checked), function() {})
+//                 .prop('disabled', checked);
+//     };
+//
+//     var setDateFieldDefaultStatus = function() {
+//         if (!$('#resource_site_capacity_sensitive').is(':checked')) {
+//             $('.resource_site_available_date').prop('disabled', true).hide();
+//         }
+//     };
+//
+//     setDateFieldDefaultStatus();
+//     $('#resource_site_capacity_sensitive').click(disableAvailability);
+//
+//     $('.closed-toggle').change(function() {
+//         var isChecked = $(this).is(':checked');
+//         $(this).parents('tr')
+//             .toggleClass('inactive', isChecked)
+//             .find('select.time-input').attr('disabled', isChecked ? "disabled" : false);
+//     });
+    
+    
+    //Toggle visibility which inputs are disabled based on the checkbox
+    $('#gold_standard_identity_alternate_village_status').change(function(){
+      
+        var isChecked = $(this).is(':checked');         
+        $('.gold_standard_identity_village_id').toggleClass('blurred');
+        $('.gold_standard_identity_alternate_village').toggleClass('blurred');                
+        $('#gold_standard_identity_village_id').attr('disabled', isChecked ? "disabled" : false);
+        $('#gold_standard_identity_alternate_village').attr('disabled', isChecked ? false : "disabled");
+    });
+    
+});
