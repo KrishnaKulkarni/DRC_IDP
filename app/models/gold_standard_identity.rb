10a5c8144e332a49a5f0ecc5f0126dedb39579c1
Krishna Kulkarni <krishna.kulkarni19@gmail.com>
2014-07-06 20:46:36 -0400

    Add import GoldStandardIdentity csv functionality

--- app/models/gold_standard_identity.rb
+++ app/models/gold_standard_identity.rb
@@ -1,4 +1,5 @@
 class GoldStandardIdentity < ActiveRecord::Base
+  require 'csv'
   
   belongs_to :household, inverse_of: :gold_standard_identities
   has_many :gold_standard_matches, inverse_of: :gold_standard_identity
@@ -27,6 +28,34 @@ class GoldStandardIdentity < ActiveRecord::Base
         end
     end
   end
+  
+  def self.import_local_csv_imports!
+    failed_csvs = []
+    Dir.entries("imports").select { |filename| /^(.)+.csv$/ =~ filename }.each do |csv_filename|
+      file = File.open("imports/#{csv_filename}")
+      succeeded = GoldStandardIdentity.import!(file)
+      failed_csvs << csv_file unless succeeded
+      file.close
+    end
+    
+    failed_csvs
+  end
+  
+  def self.import!(file)
+    succeeded = true
+    GoldStandardIdentity.transaction do
+      CSV.foreach(file.path, headers: true) do |row|
+        identity_hash = row.to_hash
+        identity_hash.delete('id')
+        unless GoldStandardIdentity.create(identity_hash)        
+          succeeded = false
+          raise "Importing error"
+        end
+      end # end CSV.foreach
+    end
+    
+    succeeded
+  end # end self.import(file)
   
   private
   def match_score(iom_identity)
