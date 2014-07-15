b3b6ffdbb07938fbc3bb70d4954b230815d4b879
Krishna Kulkarni <krishna.kulkarni19@gmail.com>
2014-07-15 00:34:54 -0400

    Add location to sign in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,22 +3,24 @@ class SessionsController < ApplicationController
     @users = User.all
     render :new
   end
-  
+
   def create
-    if (params[:username].present? && params[:computer_number].present?)
+    if (params[:username].present? && params[:computer_number].present? && params[:location].present?)
       session[:username] = User.find(params[:username]).username
       session[:computer_number] = params[:computer_number]
+      session[:location] = params[:location]
       session[:signed_in] = true
-    
+
       redirect_to new_gold_standard_identity_url
     else
       flash[:status] = "Choix valide"
       redirect_to new_session_url
     end
   end
-  
+
   def destroy
-    session[:username], session[:computer_number], session[:signed_in] = nil, nil, nil
+    session[:username], session[:computer_number] =  nil, nil
+    session[:signed_in], session[:location] = nil, nil
     redirect_to new_session_url
   end
 
