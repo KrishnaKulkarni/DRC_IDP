6031a6b479f1b364ff4a42f6f59d357defeeb9d9
Andrew Bergman <andmbergman@gmail.com>
2014-07-16 09:22:39 +0200

    tiny alterations pre-training. BUGS: DoB/Age and Other Village fields still have issues. Validations for those have been commented out to allow usability for now.

--- app/models/gold_standard_identity.rb
+++ app/models/gold_standard_identity.rb
@@ -2,7 +2,7 @@ class GoldStandardIdentity < ActiveRecord::Base
   require 'csv'
   require 'fileutils'
 
-  attr_reader :age, :head_of_household_status, :alternate_village_status
+  attr_reader :date_of_birth, :age, :head_of_household_status, :alternate_village_status
 
   belongs_to :household, inverse_of: :gold_standard_identities
   has_many :gold_standard_matches, inverse_of: :gold_standard_identity
@@ -14,9 +14,9 @@ class GoldStandardIdentity < ActiveRecord::Base
 
   # Toggle true/false to turn off/on the validation
   validates_presence_of :first_name, :last_name, :sex, :household_size,
-            :arrival_from_village, :arrival_date, :province_id, :territory_id, :village_of_origin, message: "rempliseez cette case", if: "false"
-  validates_presence_of :date_of_birth, message: "rempliseez cette case", if: "true"
-  # validates :alternate_village, presence: true, if: "alternate_village_status"
+            :arrival_from_village, :province_id, :territory_id, :village_of_origin, message: "rempliseez cette case", if: "true"
+  # BUG**: validates_presence_of :date_of_birth, message: "rempliseez cette case", if: "true"
+  # BUG**: validates :alternate_village, presence: true, if: "alternate_village_status"
   validates_presence_of :village_id, message: "rempliseez cette case", if: "alternate_village_status!='1'"
   validates_presence_of :head_of_household_first_name, :head_of_household_last_name,
             :relation_to_head_of_household, message: "rempliseez cette case", if: "head_of_household_status!='1'"
