d6a898f51603cff4f2bf588518565c4f0c79d605
Krishna Kulkarni <krishna.kulkarni19@gmail.com>
2014-07-22 01:23:40 -0400

    Enable household members to be displayed in the reconciliation form

--- app/controllers/api/iom_identities_controller.rb
+++ app/controllers/api/iom_identities_controller.rb
@@ -1,5 +1,5 @@
 class Api::IomIdentitiesController < ApplicationController
-  
+
   def create
     @iom_identity = IomIdentity.new(iom_identity_params)
     if @iom_identity.save
@@ -8,29 +8,29 @@ class Api::IomIdentitiesController < ApplicationController
       render json: @iom_identity.errors
     end
   end
-  
-  
+
+
   def destroy
     @iom_identity = IomIdentity.find(params[:id])
     render json: @iom_identity.destroy!
   end
-  
+
   def show
     @iom_identity = IomIdentity.find(params[:id])
     render json: @iom_identity
   end
-  
+
   def index
     @iom_identities = GoldStandardIdentity.find(params[:gold_standard_identity_id]).iom_identity_matches
-    
+
     render json: @iom_identities
   end
-  
+
   def temp_index
     @iom_identities = IomIdentity.all
     render json: @iom_identities
   end
-  
+
   def update
     @iom_identity = IomIdentity.find(params[:id])
     if @iom_identity.update_attributes(iom_identity_params)
@@ -39,15 +39,20 @@ class Api::IomIdentitiesController < ApplicationController
       render :json => @iom_identity.errors, :status => :unprocessable_entity
     end
   end
-  
+
+  def household
+    iom_identity = IomIdentity.find(params[:iom_identity_id])
+    render json: iom_identity.household_members
+  end
+
   private
   def iom_identity_params
     params.require(:gold_standard_identity).permit(
-    :first_name, :last_name, :alternate_name, :sex, :date_of_birth, 
+    :first_name, :last_name, :alternate_name, :sex, :date_of_birth,
     :village_id, :group_id, :collective_id, :territory_id, :province_id,
     :recorded_in_village_id, :recorded_by, :alternate_village, :village_of_origin,
     :head_of_household_first_name, :head_of_household_last_name, :relation_to_head_of_household,
     :household_size, :arrival_date, :arrival_from_village)
   end
-  
+
 end
--- app/models/iom_identity.rb
+++ app/models/iom_identity.rb
@@ -1,22 +1,29 @@
 class IomIdentity < ActiveRecord::Base
-  
+
   # belongs_to(
   # :household,
   # class_name: 'Household',
   # foreign_key: :household_id,
   # primary_key: :id
   # )
-  
+
   # Because we named the foreign key 'household_id', Rails is smart enough to know how to fill in the rest of the configuration to map this IOMIdentity to a Household
   has_many :gold_standard_matches, inverse_of: :iom_identity
   belongs_to :household, inverse_of: :iom_identities
+  has_many :household_members, through: :household, source: :iom_identities
+
   belongs_to :village, inverse_of: :iom_identities
   has_one :group, through: :village, source: :group
   has_one :collective, through: :group, source: :collective
   has_one :territory, through: :collective, source: :territory
   has_one :province, through: :territory, source: :province
-  
+
   attr_accessor :alternate_village_status
+
+  # We may want to alter this later to actually use a household model
+  def household_members
+    IomIdentity.where(household_id: self.household_id).where('id != ?', self.id)
+  end
 
   def self.import!(file)
     succeeded = true
