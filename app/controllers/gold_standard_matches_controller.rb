c51a091d99d143705559aa06b958a42fc2ef25db
Krishna Kulkarni <krishna.kulkarni19@gmail.com>
2014-07-28 02:41:16 -0400

    Set up match csv exports. Fix accuracy of status message

--- app/controllers/gold_standard_matches_controller.rb
+++ app/controllers/gold_standard_matches_controller.rb
@@ -3,9 +3,13 @@ class GoldStandardMatchesController < ApplicationController
     # gold_id =  params["gold_standard_identities"].keys.firstf
     n = 0
     params["gold_standard_matches"].keys.each do |iom_id|
-      GoldStandardMatch.create(gold_standard_identity_id: session["last_registered_identity_id"], iom_identity_id: iom_id)
-      n += 1
+      if GoldStandardMatch.create(gold_standard_identity_id: session["last_registered_identity_id"], iom_identity_id: iom_id) 
+        n += 1
+      end
     end
+
+    File.open("exports/registered_matches_#{session[:username]}_C#{session[:computer_number]}_#{session[:location]}_#{Date.today}.csv",
+       'w') { |file| file.write(GoldStandardMatch.as_csv) }
 
     flash[:status] = "You have successfully submitted #{n} matches."
     flash[:status_color] = "success-green"
