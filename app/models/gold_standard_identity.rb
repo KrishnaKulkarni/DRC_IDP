59f340d3c4c6e359226cbb31920c91cb180d37e3
Andrew Bergman <andmbergman@gmail.com>
2014-08-01 22:16:18 +0200

    remove gold_standard_identity/search_form fields that are now recorded in long form entries

--- app/models/gold_standard_identity.rb
+++ app/models/gold_standard_identity.rb
@@ -14,7 +14,7 @@ class GoldStandardIdentity < ActiveRecord::Base
 
   # Toggle true/false to turn off/on the validation
   validates_presence_of :first_name, :last_name, :sex, :household_size,
-            :arrival_from_village, :province_id, :territory_id, :village_of_origin, message: "rempliseez cette case", if: "true"
+                   :province_id, :territory_id, :village_of_origin, message: "rempliseez cette case", if: "true"
   # Still haven't solved this bug
   # validates_presence_of :date_of_birth, message: "rempliseez cette case", unless: "true"
   validates_presence_of :alternate_village, presence: true, message: "rempliseez cette case", if: "alternate_village_status=='1'"
