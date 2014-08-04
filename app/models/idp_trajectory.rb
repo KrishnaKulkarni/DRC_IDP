d5449b12a221a4b7e9e55885c866d590035fc904
Krishna Kulkarni <krishna.kulkarni19@gmail.com>
2014-08-03 23:16:04 -0400

    Solve trajectory validation issues

--- app/models/idp_trajectory.rb
+++ app/models/idp_trajectory.rb
@@ -6,9 +6,11 @@ class IdpTrajectory < ActiveRecord::Base
 
   # Toggle true/false to turn off/on the validation
   # validates_presence_of :stop_number, :province_id, :territory_id, :arrival_from_type, :mode_of_transport,
-  #                              message: "rempliseez cette case", if: "true"
+  #                            message: "rempliseez cette case", if: "true"
+  validates_presence_of :stop_number, :province_id, message: "rempliseez cette case"
+  validate :village_or_site_or_alt_village_is_present
 
-	attr_accessor :alternate_village_status
+  attr_accessor :alternate_village_status
 
   before_save :sanitize_all_blank_attributes
 
@@ -25,12 +27,20 @@ class IdpTrajectory < ActiveRecord::Base
     IdpTrajectory.where(gold_standard_identity_id: self.gold_standard_identity_id)
     .where("stop_number < ?", self.stop_number).order(stop_number: :asc)
   end
-  
+
   def all_trajectories
     IdpTrajectory.where(gold_standard_identity_id: self.gold_standard_identity_id).order(stop_number: :asc)
   end
 
   private
+
+  def village_or_site_or_alt_village_is_present
+    unless village_id.present? || site_id.present? || alternate_village.present?
+      errors.add(:village_id, "must choose a village, site, or alternate_village")
+      errors.add(:site_id, "must choose a village, site, or alternate_village")
+      errors.add(:alternate_village, "must choose a village, site, or alternate_village")
+    end
+  end
 
 # This method will make sure every attribute that is currently set to a blank string (e.g. "")
 # will instead be set to nil. This cleans up after erroneous blank string inputs that sometimes come
