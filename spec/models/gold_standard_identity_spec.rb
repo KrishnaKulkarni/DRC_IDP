3d1ae73ec162a8f3994fdf5ca7a4f4d6f15e9f9b
Krishna Kulkarni <krishna.kulkarni19@gmail.com>
2014-08-04 10:15:04 -0400

    WIP: Write tests for fixing the date inputs of GoldStandardIdentity

--- spec/models/gold_standard_identity_spec.rb
+++ spec/models/gold_standard_identity_spec.rb
@@ -2,7 +2,37 @@ require 'rails_helper'
 
 describe GoldStandardIdentity do 
 
+  describe "factory" do
+    it "should have a valid factory" do
+      expect(FactoryGirl.build(:gold_standard_identity)).to be_valid
+    end
+  end
+
   describe "validations" do
     it { should validate_presence_of(:first_name).with_message("rempliseez cette case") }
+    it { should validate_presence_of(:year_of_birth).with_message("rempliseez cette case") }
+    it { should_not validate_presence_of(:month_of_birth) }
+    it { should_not validate_presence_of(:day_of_birth) }
+    it { should_not validate_presence_of(:date_of_birth) }
+  end
+
+  describe "callbacks" do
+    describe "sanitizing blank strings" do
+      it "converts blank-string attributes to nil just before validations are run"
+    end
+
+    describe "synchronizing date_of_birth with with day_, month_, and year_of_birth" do
+
+      context "when date_of_birth is present" do
+
+      end
+
+      context "when date_of_birth is blank"
+
+      context "when all three of {day_, month_, year_of_birth} are present"
+
+      context "when at least one of {day_, month_, year_of_birth} is blank"
+
+    end
   end
 end
